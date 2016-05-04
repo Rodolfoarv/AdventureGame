14702a581ead21761a901d2dbc70ff2f5fb19eca
Rodolfo <rodolfo@ramirezvalenzuela.com>
2016-05-04 18:24:06 -0500

    Mofidied player attribute health for strength and added to_Str method

--- models/player.rb
+++ models/player.rb
@@ -11,17 +11,30 @@
 #
 # end
 
+require 'inventory'
+
 class Player
 
-  attr_accessible :strength, :wealth, :inventory, :health, :tally, :monster_tally
+  attr_accessor :strength, :wealth, :inventory, :strength, :tally, :monster_tally
 
   def initialize(name)
     @name = name
     @inventory = Inventory.new
-    @health = 60 + rand(1..100)
+    @strength = 60 + rand(1..100)
     @wealth = 30 + rand(1..100)
     @tally = 0
     @monster_tally = 0
   end
 
+  def to_s
+    %Q{
+      Your strength is #{@strength}.
+      You have $#{@wealth}.
+      Your provision sack holds #{@inventory.food} units of food.
+    }
+  end
+
 end
+
+pl = Player.new("Rodolfo")
+print pl
